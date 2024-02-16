8954ad9d0706cb0f90f832e7b7bf1f9ea63116ed
EDIVALDO LEITAO DE NETO <mandoritos.ea@gmail.com>
2024-02-16 02:03:30 +0000

    commit de novo

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,5 +1,7 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox",
+	"sap/m/MessageToast"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
@@ -10,6 +12,32 @@ sap.ui.define([
         return Controller.extend("sap.calculadora.project1.controller.View1", {
             onInit: function () {
 
+            },
+            onClear: function () {
+
+            },
+            onCalc: function() {
+                var valor1 = Number(this.getView().byId("valor1").getValue());
+                var valor2 = Number(this.getView().byId("valor2").getValue());
+                var operacao = this.getView().byId("select").getSelectedKey();
+                var result;
+
+                switch (operacao) {
+                    case '+':
+                        result = valor1 + valor2;
+                        break;
+                    case '-':
+                        result = valor1 - valor2;
+                        break;
+                    case '*':
+                        result = valor1*valor2;
+                        break;
+                    case '/':
+                        result = valor1/valor2;
+                        break;
+                }
+
+                MessageBox.success("O valor calculado foi: "+ result);
             }
         });
     });
